8e8f5e991c4394a6855f5cd7319ac0f52e2a26a0
ChuguevRoman <5tiger5xman@gmail.com>
2022-11-18 12:50:02 +0200

    Added a file where all constants are stored. Customized color theme for the whole app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './screens/input_page_screen.dart';
+import './constants.dart';
 
 void main() => runApp(BMICalculate());
 
@@ -11,6 +12,13 @@ class BMICalculate extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BMI Calculator',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: kMainColor,
+        scaffoldBackgroundColor: kMainColor,
+        appBarTheme: const AppBarTheme().copyWith(
+          color: kMainColor,
+        ),
+      ),
       home: InputPage(),
     );
   }
